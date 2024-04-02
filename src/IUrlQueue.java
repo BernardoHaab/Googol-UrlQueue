4ec4ab2f564b4565f068f69929b79d3a9cf0ddf8
Bernardo Luiz Haab <bernardohaab@gmail.com>
2024-04-02 20:46:19 +0100

    feat: Create external url queue

---
+++ src/IUrlQueue.java
@@ -0,0 +1,13 @@
+
+import java.rmi.RemoteException;
+
+public interface IUrlQueue extends java.rmi.Remote {
+
+  public String getNextUrl() throws RemoteException;
+
+  public void addUrls(java.util.List<String> urls) throws RemoteException;
+
+  public int size() throws RemoteException;
+
+  public void addUrlFirst(String url) throws RemoteException;
+}
---
+++ src/UrlQueue.java
@@ -0,0 +1,56 @@
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class UrlQueue extends UnicastRemoteObject implements IUrlQueue {
+
+  private ConcurrentLinkedDeque<String> urls = new ConcurrentLinkedDeque<String>();
+  private Set<String> knownUrls = new HashSet<>();
+
+  public static void main(String[] args) {
+    try {
+      UrlQueue urlQueue = new UrlQueue();
+      Registry registry = LocateRegistry.createRegistry(6666);
+      registry.rebind("urlQueue", urlQueue);
+
+      System.out.println("Queue ready.");
+    } catch (RemoteException re) {
+      System.out.println("Exception in UrlQueue.main: " + re);
+    }
+  }
+
+  public UrlQueue() throws RemoteException {
+    super();
+    urls.add("http://www.yahoo.com");
+  }
+
+  @Override
+  public String getNextUrl() throws RemoteException {
+    return urls.poll();
+  }
+
+  @Override
+  public void addUrls(List<String> newUrls) throws RemoteException {
+    for (String url : newUrls) {
+      if (knownUrls.add(url)) {
+        urls.add(url);
+      }
+    }
+  }
+
+  @Override
+  public int size() throws RemoteException {
+    return urls.size();
+  }
+
+  @Override
+  public void addUrlFirst(String url) throws RemoteException {
+    urls.addFirst(url);
+  }
+}
